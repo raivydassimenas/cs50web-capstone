f33c23863845f4ec30bdfbc6d89a229ceb77c3cb
Raivydas Šimėnas <raivydas.simenas@proton.me>
2023-10-15 21:33:32 +0300

    Add Post and Comment models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey("User", on_delete=models.CASCADE)
+    body = models.TextField(blank=True)
+    likes = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    author = models.ForeignKey("User", on_delete=models.CASCADE)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    body = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
