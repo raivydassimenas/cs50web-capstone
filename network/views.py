967a4d35b3cfdd29645b833351509919ab35a8f8
Raivydas Šimėnas <raivydas.simenas@proton.me>
2023-10-16 20:21:32 +0300

    Add new post form styling

--- network/views.py
+++ network/views.py
@@ -1,14 +1,15 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post, Comment
 
 
 def index(request):
-    return render(request, "network/index.html")
+    return render(request, "network/index.html", { "new_post": True })
 
 
 def login_view(request):
@@ -61,3 +62,7 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+    
+@login_required
+def new_post(request):
+    return HttpResponseRedirect(reverse("index"))
