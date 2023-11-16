d13636080cdfcc200a0fe5ced5368e0e4346a181
Raivydas Šimėnas <raivydas.simenas@proton.me>
2023-11-16 19:49:45 +0200

    Add Tailwind CSS support

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./calendarapp/templates/calendarapp/**/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
