41d0401233d8021ef4c24409734da63d7c9bc47b
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-03-14 20:09:21 +0200

    Add event adding functionality

---
+++ calendarapp/static/js/index.js
@@ -0,0 +1,33 @@
+const daysContainer = document.querySelector("#days-container");
+daysContainer.innerHTML = '';
+
+let currentDate = new Date();
+currentDate.setDate(1);
+const firstDayOfMonth = currentDate.getDay();
+
+let currentMonth = new Date().getMonth();
+currentDate.setMonth(currentMonth + 1);
+currentDate.setDate(0);
+const numDays = currentDate.getDate();
+
+const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+weekdays.forEach(day => {
+    const dayElement = document.createElement('div');
+    dayElement.classList.add('font-bold');
+    dayElement.innerText = day;
+    daysContainer.appendChild(dayElement);
+});
+
+for (let day = 1; day <= numDays; day++) {
+    const dayElement = document.createElement('a');
+    dayElement.innerText = day;
+    const date = new Date(`${currentDate.getFullYear()}-${currentDate.getMonth}-${day}`);
+    const dateString = date.toISOString();
+    const encodedDateString = encodeURIComponent(dateString);
+    dayElement.href = `/insert_event/?date=${encodedDateString}`;
+    if (day === 1) {
+        console.log(firstDayOfMonth);
+        dayElement.classList.add(`col-start-${firstDayOfMonth + 1}`);
+    }
+    daysContainer.appendChild(dayElement);
+}
