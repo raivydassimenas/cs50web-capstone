c56f650300cd370b63fd37a2d694406a822e44eb
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-04-26 06:00:37 +0300

    Correct cast to string in models.py

--- calendarapp/models.py
+++ calendarapp/models.py
@@ -32,7 +32,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
-        return self.email
+        return str(self.email)
 
 
 class Event(models.Model):
