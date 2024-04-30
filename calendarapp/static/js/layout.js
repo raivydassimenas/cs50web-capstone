2e6c3fbb505e8415e9486b12d711249e36a5a3a2
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-04-30 18:24:52 +0300

    Add responsiveness

---
+++ calendarapp/static/js/layout.js
@@ -0,0 +1,6 @@
+const mobileMenuButton = document.querySelector('#hamburger-button');
+const mobileMenu = document.querySelector('#hamburger-menu');
+
+mobileMenuButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
