b52c59fb669c9f5213d773265769531b4b79e422
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-05-03 16:58:00 +0300

    Trying to resolve TailwindCSS

--- cs50web_capstone/wsgi.py
+++ cs50web_capstone/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cs50web_final.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cs50web_capstone.settings')
 
 application = get_wsgi_application()
